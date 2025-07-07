8d84ae1bceef09f5f4f2cbc2618ebbaf67929d3b
fayez1-10-2004 <fayzm5575@gmail.com>
2025-07-08 00:19:00 +0300

    up

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Router, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import HomeCOM from './Router/HomeCOM';
 import AboutCOM from './Router/AboutCOM';
